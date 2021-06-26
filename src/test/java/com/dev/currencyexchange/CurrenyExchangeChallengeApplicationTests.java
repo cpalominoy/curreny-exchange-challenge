065afc96ea6f0184929a519aac5d95754fedfad8
Alfredo Palomino <alfredo.palomino460@gmail.com>
2021-06-25 19:29:40 -0500

    Initial Commit

---
+++ src/test/java/com/dev/currencyexchange/CurrenyExchangeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenyExchangeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
